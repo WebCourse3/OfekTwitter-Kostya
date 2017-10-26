b0fe23d0328a7a97aa305502190d16b54208c568
Kostya <sydoruk.kostya@gmail.com>
2017-10-26 09:54:09 +0300

    commit before npm init

---
+++ scripts/twitter.js
@@ -0,0 +1,5 @@
+var a = [
+    {username: 'Bobo', text: 'hello followers!'},
+    {username: 'Elvis', text: 'this exercise is really easy!'},
+    {username: 'Mimi', text: 'I want to go to sleep'}
+]
