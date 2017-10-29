3238db9e260c449b82d486e8ff077fff669fe787
Kostya <sydoruk.kostya@gmail.com>
2017-10-29 10:02:26 +0200

    updated my shieeet

--- scripts/twitter.js
+++
@@ -1,5 +0,0 @@
-var a = [
-    {username: 'Bobo', text: 'hello followers!'},
-    {username: 'Elvis', text: 'this exercise is really easy!'},
-    {username: 'Mimi', text: 'I want to go to sleep'}
-]
